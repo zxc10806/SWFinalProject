90b9ebf8812203e7159dd81691a9a182e2047ebe
Hawk <zxc10806@yahoo.com.tw>
2015-11-05 23:30:40 +0800

    initial setup

---
+++ Test/samples/triangle.cpp
@@ -0,0 +1,37 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include<cstring>
+#include<string>
+
+using namespace std;
+
+string IsTriangle(int a, int b, int c)
+{
+	if(a<=0||a>200)
+		return "Not a Triangle";
+	if(b<=0||b>200)
+		return "Not a Triangle";
+	if(c<=0||c>200)
+		return "Not a Triangle";
+	if((a<b+c)&&(b<a+c)&&(c<a+b))
+	{
+		if(a==b&&b==c)
+		{
+			return "Equilateral";
+		}
+		else if(a!=b&&a!=c&&b!=c)
+		{
+			return "Scalene";
+		}
+		else
+		{
+			return "Isosceles";
+		}
+	}
+	else
+	{
+		return "Not a Triangle";
+	}
+	return "Error";
+}
